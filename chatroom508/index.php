7a7b9cb251758cf1ca11de4b80c8df4df4b0f4c5
xiaolu31 <563535319@qq.com>
2020-05-29 16:55:00 +0800

    Update index.php

--- chatroom508/index.php
+++ chatroom508/index.php
@@ -1,6 +1,6 @@
 <?php
 $login = array('xiaowei' => 123456,'xiaoshi'=> 123456,'xiaohao'=>123456,'xiaolu'=>123456,
-				'xiaodan'=>123456,'xiaofang'=>123456,'xiaoguo'=>123456);
+				'xiaodan'=>123456,'xiaofang'=>123456,'xiaoguo'=>123456,'huangkai'=>123456);
 $name = $_GET["name"];
 $pass = $_GET["pass"];
 //验证用户名和密码是否正确
